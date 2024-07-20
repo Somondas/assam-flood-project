5cef65418a195c8e6028c7b27eaf0d36ebdef597
Somondas <somondas067@gmail.com>
2024-07-20 12:49:50 +0530

    First Commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className={`bg-custom flex flex-col items-center justify-center `}>
+      <div className="bg-transparent shadow shadow-lg  rounded-lg w-1/2 h-2/3"></div>
+    </div>
+  );
+};
+
+export default Register;
